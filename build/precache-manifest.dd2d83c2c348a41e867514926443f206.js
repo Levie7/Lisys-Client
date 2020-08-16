53c0d44fd93fb33acf60cd5583c53b819f671f23
Amelia Maya Christina <ameliamaya16@gmail.com>
2020-08-16 14:39:55 +0700

    update build

---
+++ build/precache-manifest.dd2d83c2c348a41e867514926443f206.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73ae50f6129137c08afb7c23912ed6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "604d46c627781bd2c000",
+    "url": "/static/css/29.3812ebb2.chunk.css"
+  },
+  {
+    "revision": "a02ec2519dae22b3d998",
+    "url": "/static/css/4.dcb9bbba.chunk.css"
+  },
+  {
+    "revision": "ad5d7d7ddcff7bb9ab5b",
+    "url": "/static/css/BuyPriceHistoryPage.126d4f33.chunk.css"
+  },
+  {
+    "revision": "983403f3be23abf8107a",
+    "url": "/static/css/HomePage.8ef6d8cd.chunk.css"
+  },
+  {
+    "revision": "814a239e8eac544d2a82",
+    "url": "/static/css/LoginPage.18ffa294.chunk.css"
+  },
+  {
+    "revision": "24f66e8e5be17d845400",
+    "url": "/static/css/MasterCategoryPage.126d4f33.chunk.css"
+  },
+  {
+    "revision": "3343299afcb807df1de0",
+    "url": "/static/css/MasterMedicinePage.126d4f33.chunk.css"
+  },
+  {
+    "revision": "da81e10366081807038e",
+    "url": "/static/css/MasterSupplierPage.126d4f33.chunk.css"
+  },
+  {
+    "revision": "d5576f4d4ff363f70586",
+    "url": "/static/css/MasterUoMPage.126d4f33.chunk.css"
+  },
+  {
+    "revision": "c41608dc0af40e30c390",
+    "url": "/static/css/MasterVariantPage.126d4f33.chunk.css"
+  },
+  {
+    "revision": "117bee38db0a3ea9a5cb",
+    "url": "/static/css/PurchaseListPage.7b3d5940.chunk.css"
+  },
+  {
+    "revision": "f7fd02fcb0741246984a",
+    "url": "/static/css/PurchasePaymentPage.9a91de4b.chunk.css"
+  },
+  {
+    "revision": "f8e9fd2a0a6a053b1a1e",
+    "url": "/static/css/PurchaseReturnPage.f5b9e7f0.chunk.css"
+  },
+  {
+    "revision": "c3d642b999d7ddb3f944",
+    "url": "/static/css/SalesPage.3fb7c9fd.chunk.css"
+  },
+  {
+    "revision": "ffba4ca7dc8b0abae9c0",
+    "url": "/static/css/SettingPage.79bc2a2e.chunk.css"
+  },
+  {
+    "revision": "1c707fa918712b6d84ef",
+    "url": "/static/css/SoldMedicineReportPage.dcb9bbba.chunk.css"
+  },
+  {
+    "revision": "1278232664840449ae5a",
+    "url": "/static/css/StockCardPage.126d4f33.chunk.css"
+  },
+  {
+    "revision": "04f7fd8966dc28151572",
+    "url": "/static/css/StockOpnamePage.5f25e2e1.chunk.css"
+  },
+  {
+    "revision": "cdc3c9431a80c63a5c19",
+    "url": "/static/css/main.d0e99a90.chunk.css"
+  },
+  {
+    "revision": "4a53ff6049fd490f1444",
+    "url": "/static/js/0.dc1dc9c3.chunk.js"
+  },
+  {
+    "revision": "afa74f6bd37c5412eb1b",
+    "url": "/static/js/1.8a4f723f.chunk.js"
+  },
+  {
+    "revision": "c0faf1602017b2949f45",
+    "url": "/static/js/2.a63c506f.chunk.js"
+  },
+  {
+    "revision": "604d46c627781bd2c000",
+    "url": "/static/js/29.4ddc3e2b.chunk.js"
+  },
+  {
+    "revision": "d80de78cef9f1d731f07ce309a8e3b5c",
+    "url": "/static/js/29.4ddc3e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1cc33f5638f6253cc4d",
+    "url": "/static/js/3.151a2e74.chunk.js"
+  },
+  {
+    "revision": "7b91f3fa75d42a01f4c5",
+    "url": "/static/js/30.af17dff5.chunk.js"
+  },
+  {
+    "revision": "a02ec2519dae22b3d998",
+    "url": "/static/js/4.c9997b5a.chunk.js"
+  },
+  {
+    "revision": "81289192b61e5f0e749e",
+    "url": "/static/js/5.32300b31.chunk.js"
+  },
+  {
+    "revision": "33dacf42d2a091b9143c115edecd1be8",
+    "url": "/static/js/5.32300b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "882f0aedf96dd35d1100",
+    "url": "/static/js/6.3848a467.chunk.js"
+  },
+  {
+    "revision": "ad5d7d7ddcff7bb9ab5b",
+    "url": "/static/js/BuyPriceHistoryPage.29b9d18c.chunk.js"
+  },
+  {
+    "revision": "983403f3be23abf8107a",
+    "url": "/static/js/HomePage.e6be4d36.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/static/js/HomePage.e6be4d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814a239e8eac544d2a82",
+    "url": "/static/js/LoginPage.9dcb2bd6.chunk.js"
+  },
+  {
+    "revision": "24f66e8e5be17d845400",
+    "url": "/static/js/MasterCategoryPage.3def5a59.chunk.js"
+  },
+  {
+    "revision": "3343299afcb807df1de0",
+    "url": "/static/js/MasterMedicinePage.60935edb.chunk.js"
+  },
+  {
+    "revision": "da81e10366081807038e",
+    "url": "/static/js/MasterSupplierPage.a3e463e3.chunk.js"
+  },
+  {
+    "revision": "d5576f4d4ff363f70586",
+    "url": "/static/js/MasterUoMPage.b553cf1a.chunk.js"
+  },
+  {
+    "revision": "c41608dc0af40e30c390",
+    "url": "/static/js/MasterVariantPage.1f0a6f70.chunk.js"
+  },
+  {
+    "revision": "981aef94f30def617016",
+    "url": "/static/js/MedicineAlmostDepletedReportPage.c77e7984.chunk.js"
+  },
+  {
+    "revision": "2e1e25bdbb131d45007a",
+    "url": "/static/js/MedicineAlmostExpiredReportPage.9bd50059.chunk.js"
+  },
+  {
+    "revision": "8c409f31511cbe81ec42",
+    "url": "/static/js/MedicineListReportPage.5970d9ee.chunk.js"
+  },
+  {
+    "revision": "117bee38db0a3ea9a5cb",
+    "url": "/static/js/PurchaseListPage.a9fcbb1f.chunk.js"
+  },
+  {
+    "revision": "f7fd02fcb0741246984a",
+    "url": "/static/js/PurchasePaymentPage.a72c0904.chunk.js"
+  },
+  {
+    "revision": "f8e9fd2a0a6a053b1a1e",
+    "url": "/static/js/PurchaseReturnPage.37a7a517.chunk.js"
+  },
+  {
+    "revision": "c3d642b999d7ddb3f944",
+    "url": "/static/js/SalesPage.c7185557.chunk.js"
+  },
+  {
+    "revision": "ffba4ca7dc8b0abae9c0",
+    "url": "/static/js/SettingPage.4521030b.chunk.js"
+  },
+  {
+    "revision": "1c707fa918712b6d84ef",
+    "url": "/static/js/SoldMedicineReportPage.316ebe8b.chunk.js"
+  },
+  {
+    "revision": "1278232664840449ae5a",
+    "url": "/static/js/StockCardPage.80156a8f.chunk.js"
+  },
+  {
+    "revision": "04f7fd8966dc28151572",
+    "url": "/static/js/StockOpnamePage.edb119fd.chunk.js"
+  },
+  {
+    "revision": "54f2a3c4a83a74d58968",
+    "url": "/static/js/SummarySalesReportPage.15475e2c.chunk.js"
+  },
+  {
+    "revision": "cdc3c9431a80c63a5c19",
+    "url": "/static/js/main.9ff1c088.chunk.js"
+  },
+  {
+    "revision": "cc69f5f4fc6ab55c9dca",
+    "url": "/static/js/runtime-main.8eb8abc2.js"
+  },
+  {
+    "revision": "cbf24a77845b2eb1fbc6ade16048c4ae",
+    "url": "/static/media/logo-dark.cbf24a77.png"
+  },
+  {
+    "revision": "dd715352d922511d1008111b3cc2d75f",
+    "url": "/static/media/logo-light.dd715352.png"
+  }
+]);
