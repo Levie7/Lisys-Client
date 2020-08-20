5b99e79b69e554a51b844e07f6275e09103c12af
Amelia Maya Christina <ameliamaya16@gmail.com>
2020-08-20 18:23:01 +0700

    handle-medicine-not-found-purchase-return & redirect no dashboard access

--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -1,15 +1,51 @@
 import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 
 import { Page } from 'src/app/shell/Page';
 
+import { createAuthTokenStorage } from 'src/core/graphql/auth';
+
+import { queryForm, queryList } from 'src/shared/graphql';
+import { getAccessPermissionByRoleId } from 'src/shared/graphql/Permission/schema.gql';
+import { getUserByUsername } from 'src/shared/graphql/User/schema.gql';
 import { Notification } from 'src/shared/utilities/notification';
 
 import { getLanguage } from '../SettingPage/helpers';
 import { MasterStatistic } from './containers/MasterStatistic';
 import { PurchasingGraph, SalesGraph } from './containers/DashboardGraph';
 import { notification } from './constants';
+import { Permission } from 'src/core/api';
 
-export const HomePage = () => {
+export const HomePage = ({ history }: RouteComponentProps) => {
+    let storage = createAuthTokenStorage();
+    let auth = storage.getToken();
+    let queryUser = queryForm({
+        query: getUserByUsername,
+        variables: { username: auth },
+    });
+    let queryPermission = queryList({
+        skip: !queryUser.data,
+        query: getAccessPermissionByRoleId,
+        variables: { role_id: queryUser.data?.getUserByUsername.role.id },
+    });
+
+    if (queryUser.loading || queryPermission.loading) {
+        return null;
+    }
+
+    let hasDashboardPermission = queryPermission.data?.getAccessPermissionByRoleId.find(
+        (permission: Permission) =>
+            permission.action!.name === 'Access' &&
+            permission.menu!.key === 'Dashboard' &&
+            permission.status === 'active'
+    );
+
+    if (!hasDashboardPermission) {
+        history.replace('/sales');
+
+        return null;
+    }
+
     let lang = getLanguage();
     let gretting = localStorage.getItem('greeting');
     if (gretting !== 'greet') {
